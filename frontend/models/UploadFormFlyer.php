9ee6f181620b65fea4976640813843f7dfca783e
Gonzalo <gon.pineiro@gmail.com>
2021-08-10 14:05:44 -0300

    :hammer: Solucion problema con los nombres de los archivos

--- frontend/models/UploadFormFlyer.php
+++ frontend/models/UploadFormFlyer.php
@@ -19,10 +19,19 @@ class UploadFormFlyer extends Model
         ];
     }
 
-    public function upload()
+    public function upload($folder = null)
     {
         if ($this->validate()) {
-            $this->imageFlyer->saveAs('../web/eventos/images/flyers/' . $this->imageFlyer->baseName . '.' . $this->imageFlyer->extension);
+
+            if ($folder != null) {
+                $path = '../web/eventos/images/flyers/' . $folder;
+                if (!file_exists($path)) {
+                    mkdir($path, 0755, true);
+                };
+                $this->imageFlyer->saveAs($path . '/' . $this->imageFlyer->baseName . '.' . $this->imageFlyer->extension);
+            } else {
+                $this->imageFlyer->saveAs('../web/eventos/images/flyers/' . $this->imageFlyer->baseName . '.' . $this->imageFlyer->extension);
+            }
             return true;
         } else {
             return false;
