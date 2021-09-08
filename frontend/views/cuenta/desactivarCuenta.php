7ea2865ea197dc4545d8262b20247c560f78f5e5
Norbert Stange <n0rb3r7@outlook.com>
2021-09-08 09:25:00 -0300

    Actualización diseño descativarCuenta.

--- frontend/views/cuenta/desactivarCuenta.php
+++ frontend/views/cuenta/desactivarCuenta.php
@@ -10,14 +10,14 @@ $this->title = 'Desactivar Cuenta ';
 
 $this->params['breadcrumbs'][] = 'Actualizar información';
 ?>
-<div class="profile-update container">
+<div class="profile-update container text-center">
 
-    <h1 class="text-center"> Desactivar Mi cuenta </h1>
-    <p> ¿Está seguro que desea desactivar su cuenta? Esta opción no le permitirá acceder al sitio hasta que pida un mail
-        de activación. </p>
+    <h1 class="text_muni_azul_5 mb-5"> Desactivar Mi Cuenta </h1>
+    <p> ¿Está seguro que desea desactivar su cuenta?</p>
+    <p>Esta opción no le permitirá acceder al sitio hasta que pida un <strong>email de activación</strong>. </p>
 
-    <div class="row">
-        <div class="col-lg-5 m-auto profileForm">
+    <div class="row mx-auto mt-5">
+        <div class="profileForm w-100">
             <?php $form = ActiveForm::begin(); ?>
             <div class="form-group">
                 <?= Html::submitButton('Desactivar Cuenta', ['class' => 'btn btn-primary']) ?>
