e1a943e3af565db90e9fbaa9caf9e156c5346c8d
Norbert Stange <n0rb3r7@outlook.com>
2021-09-21 12:53:56 -0300

    Favicon con https.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -17,153 +17,156 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <?php $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/ico', 'href' => Url::base('http') . '/favicon.png']); ?>
+    <?php $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/ico', 'href' => Url::base('https') . '/favicon.png']); ?>
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body>
-<?php $this->beginBody() ?>
 
-<div class="wrap mt-5">
-    <?php if (Yii::$app->requestedRoute == 'site/index' || Yii::$app->requestedRoute == null): ?>
-        <!--<header class="darkish_bg">-->
-        <div class="row">
-            <div class="col-12 text-center">
-                <div class="mb-4">
-                    <?PHP
-                    /*
+<body>
+    <?php $this->beginBody() ?>
+
+    <div class="wrap mt-5">
+        <?php if (Yii::$app->requestedRoute == 'site/index' || Yii::$app->requestedRoute == null) : ?>
+            <!--<header class="darkish_bg">-->
+            <div class="row">
+                <div class="col-12 text-center">
+                    <div class="mb-4">
+                        <?PHP
+                        /*
                     Html::img('images/logo-03.svg', [
                         'class' => 'img-fluid mx-auto d-block',
                         'style' => ['width' => '40%']
                     ]);*/
-                    ?>
-                    <h3 class="mt-2 m-auto">Aplicación Backend</h3>
-                </div>
+                        ?>
+                        <h3 class="mt-2 m-auto">Aplicación Backend</h3>
+                    </div>
 
-                <div class="col-10 m-auto">
-                    <?php
-                    $menuItems[] = [
-                        "label" => "Salir (" . Yii::$app->user->identity->nombre . " " . Yii::$app->user->identity->apellido . ")",
-                        "url" => ["/site/logout"],
-                        "linkOptions" => [
-                            "data-method" => "post",
-                            'class' => 'text-white'
-                        ]
-                    ];
+                    <div class="col-10 m-auto">
+                        <?php
+                        $menuItems[] = [
+                            "label" => "Salir (" . Yii::$app->user->identity->nombre . " " . Yii::$app->user->identity->apellido . ")",
+                            "url" => ["/site/logout"],
+                            "linkOptions" => [
+                                "data-method" => "post",
+                                'class' => 'text-white'
+                            ]
+                        ];
 
-                    echo Nav::widget([
-                        'options' => ['class' => 'btn btn-blue d-block text-center m-auto col-md-2 col-sm-12'],
-                        'items' => $menuItems,
-                    ]);
-                    ?>
+                        echo Nav::widget([
+                            'options' => ['class' => 'btn btn-blue d-block text-center m-auto col-md-2 col-sm-12'],
+                            'items' => $menuItems,
+                        ]);
+                        ?>
+                    </div>
                 </div>
             </div>
+            <!--</header>-->
+        <?php else : ?>
+            <?php
+            NavBar::begin([
+                'brandUrl' => Yii::$app->homeUrl,
+                'options' => [
+                    'class' => 'navbar navbar-expand-md navbar-white fixed-top shadow-sm',
+                ],
+            ]);
+            if (Yii::$app->user->isGuest) {
+                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+            } else {
+                //                    $menuItems[] = ['label' => 'Home', 'url' => ['/site/index']];
+                //MenuItems for ABM
+                //MenuItems ABM Permisos
+                //        $menuItems[] = [
+                //                       'label' => 'Gestor de Permisos OLD',
+                //                       'items' => [
+                //                            ['label' => 'Asignar Permisos', 'url' => ['/permission-manager/index']],
+                //                            ['label' => 'Asignar Permisos3', 'url' => ['/permission-manager/index3']],
+                //                            ['label' => 'Asignar Permisos5', 'url' => ['/permission-manager/index5']],
+                //                            ['label' => 'Asignar Permisos7', 'url' => ['/permission-manager/index7']],
+                //                            ],
+                //                        ];
+                //ABM Usuario
+                $menuItems[] = ['label' => 'Usuarios', 'url' => ['/usuario/index']];
+                //ABM Eventos
+                $menuItems[] = [
+                    'label' => 'Eventos',
+                    'items' => [
+                        ['label' => 'Listado Eventos', 'url' => ['/evento/index']],
+                        ['label' => 'Solicitudes de Aval', 'url' => ['/solicitud-aval/solicitudes-de-aval']],
+                    ],
+                ];
+                //ABM ModalidadEventos
+                $menuItems[] = ['label' => 'Modalidades', 'url' => ['/modalidad-evento/index']];
+                //ABM ModalidadEventos
+                $menuItems[] = ['label' => 'Categorias', 'url' => ['/categoria-evento/index']];
+                //MenuItems ABM Permisos
+                $menuItems[] = [
+                    'label' => 'Permisos',
+                    'items' => [
+                        ['label' => 'Crear Permiso', 'url' => ['/permission/create-permiso']],
+                        ['label' => 'Asignar Permisos', 'url' => ['/permission/asignar-permisos']],
+                        ['label' => 'Listado Permisos', 'url' => ['/permission/index']],
+                        ['label' => 'Eliminar Permiso', 'url' => ['/permission/remove-permiso']],
+                    ],
+                ];
+                $menuItems[] = [
+                    'label' => 'Roles',
+                    'items' => [
+                        ['label' => 'Crear Rol', 'url' => ['/rol/create-rol']],
+                        ['label' => 'Listado Roles', 'url' => ['/rol/index']],
+                    ],
+                ];
+
+
+                //                $menuItems[] = ['label' => 'Gestionar Eventos', 'url' => ['/evento/index']];
+                //Logout
+                $menuItems[] = [
+                    "label" => "Salir (" . Yii::$app->user->identity->nombre . ")",
+                    "url" => ["/site/logout"],
+                    "linkOptions" => [
+                        "data-method" => "post",
+                    ]
+                ];
+            }
+            echo Nav::widget([
+                'options' => ['class' => 'navbar-nav navbar-collapse justify-content-end'],
+                'items' => $menuItems,
+            ]);
+            NavBar::end();
+            ?>
+        <?php endif; ?>
+
+
+        <!--<div class="container-fluid">-->
+        <div class="container p-3">
+            <?=
+            Breadcrumbs::widget([
+                'itemTemplate' => "\n\t<li class=\"breadcrumb-item\"><i>{link}</i></li>\n", // template for all links
+                'activeItemTemplate' => "\t<li class=\"breadcrumb-item active\">{link}</li>\n", // template for the active link
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ])
+            ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
         </div>
-        <!--</header>-->
-    <?php else: ?>
-        <?php
-        NavBar::begin([
-            'brandUrl' => Yii::$app->homeUrl,
-            'options' => [
-                'class' => 'navbar navbar-expand-md navbar-white fixed-top shadow-sm',
-            ],
-        ]);
-        if (Yii::$app->user->isGuest) {
-            $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-        } else {
-//                    $menuItems[] = ['label' => 'Home', 'url' => ['/site/index']];
-            //MenuItems for ABM
-            //MenuItems ABM Permisos
-//        $menuItems[] = [
-//                       'label' => 'Gestor de Permisos OLD',
-//                       'items' => [
-//                            ['label' => 'Asignar Permisos', 'url' => ['/permission-manager/index']],
-//                            ['label' => 'Asignar Permisos3', 'url' => ['/permission-manager/index3']],
-//                            ['label' => 'Asignar Permisos5', 'url' => ['/permission-manager/index5']],
-//                            ['label' => 'Asignar Permisos7', 'url' => ['/permission-manager/index7']],
-//                            ],
-//                        ];
-            //ABM Usuario
-            $menuItems[] = ['label' => 'Usuarios', 'url' => ['/usuario/index']];
-            //ABM Eventos
-            $menuItems[] = [
-                'label' => 'Eventos',
-                'items' => [
-                    ['label' => 'Listado Eventos', 'url' => ['/evento/index']],
-                    ['label' => 'Solicitudes de Aval', 'url' => ['/solicitud-aval/solicitudes-de-aval']],
-                ],
-            ];
-            //ABM ModalidadEventos
-            $menuItems[] = ['label' => 'Modalidades', 'url' => ['/modalidad-evento/index']];
-            //ABM ModalidadEventos
-            $menuItems[] = ['label' => 'Categorias', 'url' => ['/categoria-evento/index']];
-            //MenuItems ABM Permisos
-            $menuItems[] = [
-                'label' => 'Permisos',
-                'items' => [
-                    ['label' => 'Crear Permiso', 'url' => ['/permission/create-permiso']],
-                    ['label' => 'Asignar Permisos', 'url' => ['/permission/asignar-permisos']],
-                    ['label' => 'Listado Permisos', 'url' => ['/permission/index']],
-                    ['label' => 'Eliminar Permiso', 'url' => ['/permission/remove-permiso']],
-                ],
-            ];
-            $menuItems[] = [
-                'label' => 'Roles',
-                'items' => [
-                    ['label' => 'Crear Rol', 'url' => ['/rol/create-rol']],
-                    ['label' => 'Listado Roles', 'url' => ['/rol/index']],
-                ],
-            ];
-
-
-//                $menuItems[] = ['label' => 'Gestionar Eventos', 'url' => ['/evento/index']];
-            //Logout
-            $menuItems[] = [
-                "label" => "Salir (" . Yii::$app->user->identity->nombre . ")",
-                "url" => ["/site/logout"],
-                "linkOptions" => [
-                    "data-method" => "post",
-                ]
-            ];
-        }
-        echo Nav::widget([
-            'options' => ['class' => 'navbar-nav navbar-collapse justify-content-end'],
-            'items' => $menuItems,
-        ]);
-        NavBar::end();
-        ?>
-    <?php endif; ?>
-
-
-    <!--<div class="container-fluid">-->
-    <div class="container p-3">
-        <?=
-        Breadcrumbs::widget([
-            'itemTemplate' => "\n\t<li class=\"breadcrumb-item\"><i>{link}</i></li>\n", // template for all links
-            'activeItemTemplate' => "\t<li class=\"breadcrumb-item active\">{link}</li>\n", // template for the active link
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ])
-        ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+        <!--</div>-->
     </div>
-    <!--</div>-->
-</div>
 
-<footer class="darkish_bg text-white footer fixed-bottom">
-    <!--<footer class="footer">-->
-    <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-    </div>
-</footer>
+    <footer class="darkish_bg text-white footer fixed-bottom">
+        <!--<footer class="footer">-->
+        <div class="container">
+            <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+        </div>
+    </footer>
 
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
