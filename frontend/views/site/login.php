f19fa845e7dd785ff26c1a560d8d5aa1fcbacc54
Norbert Stange <n0rb3r7@outlook.com>
2021-09-23 10:45:29 -0300

    Logo muni en login y limpieza de imágenes viejas.

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -1,31 +1,37 @@
 <?php
 /* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
 
-/* @var $model LoginForm */
-
-use common\models\LoginForm;
-use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
 
 $this->title = 'Iniciar sesión';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login container">
-    <h1 class="text-center mb-2"><?= Html::encode($this->title) ?></h1>
     <div class="row">
-        <div class="col-lg-5 m-auto">
+        <div class="col-12 col-md-8 mx-auto">
+            <div class="mx-auto pb-4" style="width:250px">
+                <?= Html::img('@web/images/logo-03.svg', ['style' => 'width:250px']) ?>
+            </div>
+            <h4 class="text-center pb-3 text_muni_azul_5">Iniciar Sesión</h4>
             <form action="">
-                <div class="form-group">
+                <div class="form-group col-12 col-md-6 mx-auto">
                     <label for="dniOrEmail">DNI o Correo Electrónico</label>
                     <input type="text" class="form-control" id="dniOrEmail">
                 </div>
-                <div class="form-group">
+                <div class="form-group col-12 col-md-6 mx-auto">
                     <label for="password">Clave</label>
                     <input type="password" class="form-control" id="password">
                 </div>
-                <button type="submit" class="btn btn-primary">Ingresar</button>
+                <div class="form-group col-12 col-md-6 mx-auto">
+                    <button type="submit" class="btn btn-primary login-btn">Ingresar</button>
+                    <div class="pt-3"> <a href="https://weblogin.muninqn.gov.ar">Quiero registrarme</a>
+
+                    </div>
+
             </form>
         </div>
+
     </div>
+
+</div>
 </div>
