6fca2ab3b646829b27fb7356525041d80cbb06e9
Norbert Stange <n0rb3r7@outlook.com>
2021-09-24 10:50:06 -0300

    Cambio de organizador en las cards de eventos en index.
    Se muestra la secretaría o unidad.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -79,10 +79,12 @@ $this->title = 'Muni Eventos';
                             <?= Html::a(Html::img(Url::base('') . '/' . Html::encode($evento["imgLogo"]), ["class" => "card-img-top"]), ['/eventos/ver-evento/' . $evento->nombreCortoEvento]) ?>
                             <div class='card-body'>
                                 <h4 class='card-title font-weight-bold text_muni_azul_5'><?= Html::encode($evento["nombreEvento"]) ?></h4>
-                                <h5 class='card-title text-dark'><?= Html::encode("Organizador: " . $evento["idUsuario0"]["nombre"] . " " . $evento["idUsuario0"]["apellido"]) ?></h5>
-                                <h5 class='card-title text-muted'><?= Html::encode(date('d/m/Y', strtotime($evento["fechaInicioEvento"]))) ?> <?PHP if ($evento["horaInicioEvento"] != null && $evento["horaFinEvento"] != null) {
-                                                                                                                                                    echo "de " . $evento["horaInicioEvento"] . " hasta " . $evento["horaFinEvento"];
-                                                                                                                                                } ?></h5>
+                                <p class='card-title text-dark'>Organiza:</p>
+                                <h5 class='card-title text-dark'><?= Html::encode($evento["secretariaEvento"]) ?></h5>
+                                <h5 class='card-title text-muted'><?= Html::encode(date('d/m/Y', strtotime($evento["fechaInicioEvento"]))) ?>
+                                    <?PHP if ($evento["horaInicioEvento"] != null && $evento["horaFinEvento"] != null) {
+                                        echo "de " . $evento["horaInicioEvento"] . " hasta " . $evento["horaFinEvento"];
+                                    } ?></h5>
                                 <hr>
                                 <p class='card-text font-weight-light'><?= Html::encode($evento["lugar"]) ?></p>
                                 <p class='card-text font-weight-light'><?= Html::decode(strtok(wordwrap(strip_tags($evento["descripcionEvento"]), 250, "...\n"), "\n")) ?> </p>
